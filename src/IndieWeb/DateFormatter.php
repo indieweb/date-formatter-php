6be52b574c6d394c4610c21fb0c91dc32ff40251
Aaron Parecki <aaron@parecki.com>
2013-07-05 17:27:33 -0700

    Initial checkin of code basically copied from what was in p3k with minor changes. Test suite demonstrating the correctness except for when the year is different.

---
+++ src/IndieWeb/DateFormatter.php
@@ -0,0 +1,60 @@
+<?php
+class DateFormatter {
+  public static $timezoneOffsetFormat = 'O';  // -0700
+  // public static $timezoneOffsetFormat = 'P';  // -07:00
+
+  public static function format($startISO, $endISO=false, $html=true) {
+    ob_start();
+
+    $start = DateTime::createFromFormat('Y-m-d\TH:i:sT', $startISO);
+    if($endISO)
+      $end = DateTime::createFromFormat('Y-m-d\TH:i:sT', $endISO);
+
+    if($start === false || ($endISO && $end === false)) {
+      return null;
+    }
+
+    if($endISO) {
+      if($start->format('F') == $end->format('F')) { 
+        // Same month
+        if($start->format('j') == $end->format('j')) {
+          // Same month and day
+          if($html) echo '<time class="dt-start" datetime="' . $startISO . '">';
+          echo $start->format('F j, Y');
+          echo ' from ';
+          echo $start->format('g:ia');
+          if($html) echo '</time>';
+          echo ' to ';
+          if($html) echo '<time class="dt-end" datetime="' . $endISO . '">';
+          echo $end->format('g:ia (O)');
+          if($html) echo '</time>';
+        } else {
+          // Same month, different day
+          if($html) echo '<time class="dt-start" datetime="' . $startISO . '">';
+          echo $start->format('F j, Y');
+          echo ' at ';
+          echo $start->format('g:ia');
+          if($html) echo '</time>';
+          echo ' until ';
+          if($html) echo '<time class="dt-end" datetime="' . $endISO . '">';
+          echo $end->format('M j \a\t g:ia (O)');
+          if($html) echo '</time>';
+        }
+      } else {
+        // Different month
+        if($html) echo '<time class="dt-start" datetime="' . $startISO . '">';
+        echo $start->format('F j, Y g:ia');
+        if($html) echo '</time>';
+        echo ' until ';
+        if($html) echo '<time class="dt-end" datetime="' . $endISO . '">';
+        echo $end->format('F j \a\t g:ia (O)');
+        if($html) echo '</time>';
+      }
+    } else {
+      if($html) echo '<time class="dt-start" datetime="' . $startISO . '">';
+      echo $start->format('F j, Y \a\t g:ia (O)');
+      if($html) echo '</time>';
+    }
+    return ob_get_clean();
+  }
+}
---
+++ tests/BasicTest.php
@@ -0,0 +1,42 @@
+<?php
+class BasicTest extends PHPUnit_Framework_TestCase {
+
+  public function setUp() {
+    date_default_timezone_set('UTC');
+    DateFormatter::$timezoneOffsetFormat = 'O';
+  }
+
+  private function _testEquals($expected, $start, $end=false) {
+    $formatted = DateFormatter::format($start, $end, false);
+    $this->assertEquals($expected, $formatted);
+  }
+
+  public function testInvalidStartDate() {
+    $this->_testEquals(null, 'invalid');
+  }
+
+  public function testStartDateOnly() {
+    $this->_testEquals('October 31, 2013 at 7:10pm (-0700)', '2013-10-31T19:10:00-07:00');
+  }
+
+  public function testSameYearSameMonthSameDayDifferentTime() {
+    $this->_testEquals('October 8, 2013 from 7:00am to 5:00pm (-0700)', '2013-10-08T07:00:00-07:00', '2013-10-08T17:00:00-07:00');
+  }
+
+  public function testSameYearSameMonthDifferentDayDifferentTime() {
+    $this->_testEquals('October 8, 2013 at 7:00am until Oct 10 at 5:00pm (-0700)', '2013-10-08T07:00:00-07:00', '2013-10-10T17:00:00-07:00');
+  }
+
+  public function testSameYearDifferentMonthDifferentDayDifferentTime() {
+    $this->_testEquals('August 31, 2013 7:00am until September 1 at 5:00pm (-0700)', '2013-08-31T07:00:00-07:00', '2013-09-01T17:00:00-07:00');
+  }
+
+  public function testDifferentYearDifferentMonthDifferentDayDifferentTime() {
+    $this->_testEquals('December 31, 2013 7:00am until January 1 at 5:00pm (-0700)', '2013-12-31T07:00:00-07:00', '2013-01-01T17:00:00-07:00');
+  }
+
+  public function testDifferentYearSameMonthSameDayDifferentTime() {
+    $this->_testEquals('September 1, 2013 7:00am until September 1, 2014 at 5:00pm (-0700)', '2013-09-01T07:00:00-07:00', '2014-09-01T17:00:00-07:00');
+  }
+  
+}
